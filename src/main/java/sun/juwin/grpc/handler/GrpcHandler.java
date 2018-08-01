237e85b78f74488be73a1d9611f2b851dd42d222
sunqinwen <sunqinwen@bilibili.com>
2018-08-01 16:25:14 +0800

    新增grpc生成代码模块

---
+++ src/main/java/sun/juwin/grpc/handler/GrpcHandler.java
@@ -0,0 +1,14 @@
+/**
+ * Bilibili.com Inc.
+ * Copyright (c) 2009-2018 All Rights Reserved.
+ */
+package sun.juwin.grpc.handler;
+
+/**
+ *
+ * @author sunqinwen
+ * @version \: GrpcHandler.java,v 0.1 2018-08-01 16:24 
+ *
+ */
+public class GrpcHandler {
+}
