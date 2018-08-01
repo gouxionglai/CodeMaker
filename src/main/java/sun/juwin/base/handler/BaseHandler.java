380a946b1885c1f784d5f5a8f41fcffb528e89f8
sunqinwen <sunqinwen@bilibili.com>
2018-08-01 16:32:57 +0800

    判空

--- src/main/java/sun/juwin/base/handler/BaseHandler.java
+++ src/main/java/sun/juwin/base/handler/BaseHandler.java
@@ -1,9 +1,9 @@
 /**
-
  * Copyright (c) 2009-2017 All Rights Reserved.
  */
 package sun.juwin.base.handler;
 
+import com.google.common.base.Strings;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
@@ -41,17 +41,17 @@ public class BaseHandler {
     private String targetServiceImplPath;
     private String targetControllerPath;
 
-    public void baseHandler(String baseProPath, String tableName, String className) throws Exception{
+    public void baseHandler(String baseProPath, String tableName, String className) throws Exception {
         String path = BaseHandler.class.getResource("").toString();
         //取得模板根目录
-        String temPath = path.substring(path.indexOf(START_WORD)+6, path.indexOf(PRO_NAME))+TEMPLATE_PATH;
+        String temPath = path.substring(path.indexOf(START_WORD) + 6, path.indexOf(PRO_NAME)) + TEMPLATE_PATH;
         //Velocity引擎及初始化相关
         VelocityEngine ve = new VelocityEngine();
-        Properties p =new Properties();
+        Properties p = new Properties();
         p.put(Velocity.FILE_RESOURCE_LOADER_PATH, temPath);
         ve.init(p);
         //引入model层模板
-        Template modelVm = ve.getTemplate("model.vm","utf-8");
+        Template modelVm = ve.getTemplate("model.vm", "utf-8");
         //引入dao层相关的模板
         Template mapperVm = ve.getTemplate("mapper.vm", "utf-8");
         Template mapperXmlVm = ve.getTemplate("mapper-xml.vm", "utf-8");
@@ -63,83 +63,94 @@ public class BaseHandler {
         VelocityContext context = new VelocityContext();
         Table table = new Table();
         TaskModel model = table.getTaskModel(tableName);
-        if(model != null && model.getModels() != null && model.getModels().size() > 0){
+        if (model != null && model.getModels() != null && model.getModels().size() > 0) {
             //初始化一些必要的路径
-            this.setTargetPath(baseProPath, className);
+            setTargetPath(baseProPath);
             context.put("model", model);
             context.put("className", className);
-            context.put("modelPath", this.pointModelPath);
-            context.put("mapperPath", this.pointMapperPath);
-            context.put("servicePath", this.pointServicePath);
-            context.put("serviceImplPath", this.pointServiceImplPath);
-            context.put("controllerPath", this.pointControllerPath);
+            context.put("modelPath", pointModelPath);
+            context.put("mapperPath", pointMapperPath);
+            context.put("servicePath", pointServicePath);
+            context.put("serviceImplPath", pointServiceImplPath);
+            context.put("controllerPath", pointControllerPath);
             context.put("tableName", tableName);
             context.put("lowerClassName", className.substring(0, 1).toLowerCase() + className.substring(1));
 
-            StringWriter modelStr = new StringWriter();
-            modelVm.merge(context, modelStr);
-            System.out.println(this.targetModelPath+"/"+className+".java");
-            setContentToPath(this.targetModelPath, className+".java", modelStr.toString());
+            if (!Strings.isNullOrEmpty(targetModelPath)) {
+                StringWriter modelStr = new StringWriter();
+                modelVm.merge(context, modelStr);
+                System.out.println(targetModelPath + "/" + className + ".java");
+                setContentToPath(targetModelPath, className + ".java", modelStr.toString());
+            }
 
-            StringWriter mapperStr = new StringWriter();
-            mapperVm.merge(context, mapperStr);
-            System.out.println(this.targetMapperPath+"/"+className+"Mapper.java");
-            setContentToPath(this.targetMapperPath, className+"Mapper.java", mapperStr.toString());
+            if (!Strings.isNullOrEmpty(targetMapperPath)) {
+                StringWriter mapperStr = new StringWriter();
+                mapperVm.merge(context, mapperStr);
+                System.out.println(targetMapperPath + "/" + className + "Mapper.java");
+                setContentToPath(targetMapperPath, className + "Mapper.java", mapperStr.toString());
+            }
 
-            StringWriter mapperXmlStr = new StringWriter();
-            mapperXmlVm.merge(context, mapperXmlStr);
-            System.out.println(this.targetMapperPath+"/"+className+"Mapper.xml");
-            setContentToPath(this.targetMapperPath, className+"Mapper.xml", mapperXmlStr.toString());
+            if (!Strings.isNullOrEmpty(targetMapperPath)) {
+                StringWriter mapperXmlStr = new StringWriter();
+                mapperXmlVm.merge(context, mapperXmlStr);
+                System.out.println(targetMapperPath + "/" + className + "Mapper.xml");
+                setContentToPath(targetMapperPath, className + "Mapper.xml", mapperXmlStr.toString());
+            }
 
-            StringWriter serviceStr = new StringWriter();
-            serviceVm.merge(context, serviceStr);
-            System.out.println(this.targetServicePath+"/"+className+"Service.java");
-            setContentToPath(this.targetServicePath, className+"Service.java", serviceStr.toString());
+            if (!Strings.isNullOrEmpty(targetServicePath)) {
+                StringWriter serviceStr = new StringWriter();
+                serviceVm.merge(context, serviceStr);
+                System.out.println(targetServicePath + "/" + className + "Service.java");
+                setContentToPath(targetServicePath, className + "Service.java", serviceStr.toString());
+            }
 
+            if (!Strings.isNullOrEmpty(targetServiceImplPath)) {
+                StringWriter serviceImplStr = new StringWriter();
+                serviceImplVm.merge(context, serviceImplStr);
+                System.out.println(targetServiceImplPath + "/" + className + "ServiceImpl.java");
+                setContentToPath(targetServiceImplPath, className + "ServiceImpl.java", serviceImplStr.toString());
+            }
 
-            StringWriter serviceImplStr = new StringWriter();
-            serviceImplVm.merge(context, serviceImplStr);
-            System.out.println(this.targetServiceImplPath+"/"+className+"ServiceImpl.java");
-            setContentToPath(this.targetServiceImplPath, className+"ServiceImpl.java", serviceImplStr.toString());
-
-            StringWriter controllerStr = new StringWriter();
-            controllerVm.merge(context, controllerStr);
-            System.out.println(this.targetControllerPath+"/"+className+"Controller.java");
-            setContentToPath(this.targetControllerPath, className+"Controller.java", controllerStr.toString());
+            if (!Strings.isNullOrEmpty(targetControllerPath)) {
+                StringWriter controllerStr = new StringWriter();
+                controllerVm.merge(context, controllerStr);
+                System.out.println(targetControllerPath + "/" + className + "Controller.java");
+                setContentToPath(targetControllerPath, className + "Controller.java", controllerStr.toString());
+            }
         }
     }
 
     /**
      * 生成java路径
      */
-    public void setPointPath(PathModel pointPath){
-        this.pointModelPath = pointPath.getModelPath();
-        this.pointMapperPath = pointPath.getMapperPath();
-        this.pointServicePath = pointPath.getServicePath();
-        this.pointServiceImplPath = pointPath.getServiceImplPath();
-        this.pointControllerPath = pointPath.getControllerPath();
+    public void setPointPath(PathModel pointPath) {
+        pointModelPath = pointPath.getModelPath();
+        pointMapperPath = pointPath.getMapperPath();
+        pointServicePath = pointPath.getServicePath();
+        pointServiceImplPath = pointPath.getServiceImplPath();
+        pointControllerPath = pointPath.getControllerPath();
     }
 
     /**
      * 生成最终项目文件的去向
      */
-    private void setTargetPath(String baseProPath, String className){
-        this.targetModelPath = baseProPath+"/"+this.pointModelPath.replace(".", "/");
-        this.targetMapperPath = baseProPath+"/"+this.pointMapperPath.replace(".", "/");
-        this.targetServicePath = baseProPath+"/"+this.pointServicePath.replace(".", "/");
-        this.targetServiceImplPath = baseProPath+"/"+this.pointServiceImplPath.replace(".", "/");
-        this.targetControllerPath = baseProPath+"/"+this.pointControllerPath.replace(".", "/");
+    private void setTargetPath(String baseProPath) {
+        targetModelPath = baseProPath + "/" + pointModelPath.replace(".", "/");
+        targetMapperPath = baseProPath + "/" + pointMapperPath.replace(".", "/");
+        targetServicePath = baseProPath + "/" + pointServicePath.replace(".", "/");
+        targetServiceImplPath = baseProPath + "/" + pointServiceImplPath.replace(".", "/");
+        targetControllerPath = baseProPath + "/" + pointControllerPath.replace(".", "/");
     }
 
     /**
      * 模板内容推送
      */
-    private void setContentToPath(String dirPath, String fileName, String str) throws Exception{
+    private void setContentToPath(String dirPath, String fileName, String str) throws Exception {
         File file = new File(dirPath);
-        if(!file.exists()){
+        if (!file.exists()) {
             file.mkdirs();
         }
-        FileOutputStream of = new FileOutputStream(dirPath+"/"+fileName);
+        FileOutputStream of = new FileOutputStream(dirPath + "/" + fileName);
         of.write(str.getBytes("UTF-8"));
         of.flush();
         of.close();
